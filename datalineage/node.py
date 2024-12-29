1a63a87190aa994bd0be75280ed883aea8047789
viplazylmht <viplazylmht@gmail.com>
2024-12-29 13:16:40 +0700

    feat(node): add node_id, node upstreams, and change node to json format

--- datalineage/node.py
+++ datalineage/node.py
@@ -1,9 +1,10 @@
 from copy import deepcopy
 from enum import Enum
 from dataclasses import dataclass, field
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Dict
 from sqlglot import exp
 import json
+from hashlib import sha256
 
 
 class NodeType(str, Enum):
@@ -30,14 +31,46 @@ class Node:
     _parent: Optional["Node"] = None
     node_type: Optional[NodeType] = None
     children: List["Node"] = field(default_factory=list)
+    upstreams: List["Node"] = field(default_factory=list)
     downstreams: List["Node"] = field(default_factory=list)
 
+    @property
+    def node_id(self) -> int:
+        return int.from_bytes(
+            sha256(json.dumps(self._build_node_dict()).encode()).digest()[:8],
+            byteorder="big",
+            signed=False,
+        )
+
     def add_downstream(self, downstream: "Node") -> "Node":
-        self.downstreams.append(downstream)
+        if not self.has_downstream(downstream):
+            self.downstreams.append(downstream)
+
+        if not downstream.has_upstream(self):
+            downstream.add_upstream(self)
+
         return self
 
     def has_downstream(self, node: "Node") -> bool:
-        return node in self.downstreams
+        node_id = node.node_id
+        return any(map(lambda n: n.node_id == node_id, self.downstreams))
+
+    def add_upstream(self, upstream: "Node") -> "Node":
+        if not self.has_upstream(upstream):
+            self.upstreams.append(upstream)
+
+        if not upstream.has_downstream(self):
+            upstream.add_downstream(self)
+
+        return self
+
+    def has_upstream(self, node: "Node") -> bool:
+        node_id = node.node_id
+        return any(map(lambda n: n.node_id == node_id, self.upstreams))
+
+    def has_child(self, child: "Node") -> bool:
+        child_id = child.node_id
+        return any(map(lambda n: n.node_id == child_id, self.children))
 
     @property
     def parent(self):
@@ -52,7 +85,7 @@ class Node:
         self._parent = None
 
     def add_child(self, child: "Node") -> "Node":
-        if child not in self.children:
+        if not self.has_child(child):
             if child.parent:
                 raise ValueError("the child is already occupied by another parent.")
 
@@ -71,22 +104,49 @@ class Node:
 
         return resolved_child
 
-    def walk(self):
+    def walk(self, downstream=True, upstream=True):
         """Pre-Order DFS"""
         yield self
 
-        for d in self.downstreams:
-            yield from d.walk()
+        if downstream:
+            yield from self.downstream_walk(reverse=False)
 
-    def reversed_walk(self):
-        """Post-Order DFS"""
+        if upstream:
+            yield from self.upstream_walk(reverse=False)
+
+    def downstream_walk(self, reverse=False):
+        """Pre-Order DFS"""
         for d in self.downstreams:
-            yield from d.reversed_walk()
+            if not reverse:
+                yield d
+                yield from d.downstream_walk(reverse=reverse)
+            else:
+                yield from d.downstream_walk(reverse=reverse)
+                yield d
+
+    def upstream_walk(self, reverse=False):
+        """Pre-Order DFS"""
+        for u in self.upstreams:
+            if not reverse:
+                yield u
+                yield from u.upstream_walk(reverse=reverse)
+            else:
+                yield from u.upstream_walk(reverse=reverse)
+                yield u
+
+    def reversed_walk(self, downstream=True, upstream=True):
+        """Post-Order DFS"""
+
+        if downstream:
+            yield from self.downstream_walk(reverse=True)
+
+        if upstream:
+            yield from self.upstream_walk(reverse=True)
 
         yield self
 
-    def to_json_dict(self) -> dict:
-        result = dict(
+    def _build_node_dict(self) -> dict:
+        return dict(
             name=str(self.name),
             expression=self.expression.sql(),
             generated_expression=self.generated_expression.sql()
@@ -98,10 +158,45 @@ class Node:
             else self.node_type
             if self.node_type
             else None,
-            children=[child.to_json_dict() for child in self.children],
-            downstreams=[downstream.to_json_dict() for downstream in self.downstreams],
+            parent_id=self.parent.node_id if self.parent else None,
         )
-        return result
+
+    def to_json_dict(self) -> dict:
+        nodes: Dict[int, Dict] = {}
+        edges: Dict[int, List[int]] = {}
+        children: Dict[int, List[int]] = {}
+
+        for node in self.walk():
+            node_id = node.node_id
+
+            if node_id not in nodes:
+                nodes[node_id] = node._build_node_dict()
+
+                for child in node.children:
+                    child_id = child.node_id
+                    if child_id not in nodes:
+                        nodes[child_id] = child._build_node_dict()
+
+                    if node_id not in children:
+                        children[node_id] = [child_id]
+                    elif child_id not in children[node_id]:
+                        children[node_id].append(child_id)
+
+                for downstream in node.downstreams:
+                    downstream_id = downstream.node_id
+                    if node_id not in edges:
+                        edges[node_id] = [downstream_id]
+                    elif downstream_id not in edges[node_id]:
+                        edges[node_id].append(downstream_id)
+
+                for upstream in node.upstreams:
+                    upstream_id = upstream.node_id
+                    if upstream_id not in edges:
+                        edges[upstream_id] = [node_id]
+                    elif node_id not in edges[upstream_id]:
+                        edges[upstream_id].append(node_id)
+
+        return dict(nodes=nodes, edges=edges, children=children)
 
     def __str__(self) -> str:
         return "Node<{}>".format(json.dumps(self.to_json_dict()))
